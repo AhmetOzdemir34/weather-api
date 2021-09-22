6ea82e11d0fda89f576ac64837dc8488589f1cf2
ahmetozdemir34 <punchego.dopinghome@gmail.com>
2021-09-22 03:14:54 +0300

    new responsive design

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
-import React, { useEffect, useState } from 'react'
+import React, { useState } from 'react'
 import './App.css';
-import axios from 'axios';
 
 const api = {
   key : "24e6732d739a08064aee546e1e81e86b",
@@ -38,22 +37,29 @@ function App() {
   }
   return (
     <div className="main">
-        <div className="search">
-          <input type="text" onChange={(e)=> setQuery(e.target.value)} 
-          onKeyPress={(e)=> pressEnter(e)} 
-          placeholder="enter a city name"/>
-        </div>
-        <div className="date">{createDate(new Date())}</div>
-        {typeof weather.main != "undefined" ?(<div className="w-holder">
-          <h2 className="w-name">{weather.name}, {weather.sys.country}</h2>
-          <h3 className="w-c">{Math.round(weather.main.temp-273.15)}°C</h3>
-          <div className="extras">
-          <h3 className="w-desc">DESCR: {weather.weather[0].description}</h3>
-          <h3 className="hissedilen">Feels like: {Math.round(weather.main.feels_like-273.15)}°C</h3>
-          <h3 className="min-deg">Min degree: {Math.round(weather.main.temp_min-273.15)}°C</h3>
-          <h3 className="max-deg">Max degree: {Math.round(weather.main.temp_max-273.15)}°C</h3>
+      <div className="w-full" style={{textAlign:"center"}}>
+          <div className="search">
+            <input type="text" onChange={(e)=> setQuery(e.target.value)} 
+            onKeyPress={(e)=> pressEnter(e)} 
+            placeholder="enter a location name"/>
           </div>
-        </div>) : ("")}
+          
+          <div className="date">{createDate(new Date())}</div>
+      </div>
+
+      <div className="w-full" style={{textAlign:"center"}}>
+        {typeof weather.main != "undefined" ? (<div className="w-holder">
+          <h2 className="w-name">{weather.name}, {weather.sys.country}</h2>
+          <div className="degree">{Math.round(weather.main.temp-273.15)}°C</div>
+          <div className="extras">
+            <div className="desc">Description : {weather.weather[0].description}</div>
+            <div className="alt-flex">
+              <div className="divo" style={{backgroundColor:"rgba(0,0,255,.5)"}}>MIN <br/> {Math.round(weather.main.temp_min-273.15)}°C</div>
+              <div className="divo" style={{backgroundColor:"rgba(255,0,0,.5)"}}>MAX <br/> {Math.round(weather.main.temp_max-273.15)}°C</div>
+            </div>
+          </div>
+        </div>): <h1>Not found!</h1>}
+      </div>
     </div>
   );
 }
